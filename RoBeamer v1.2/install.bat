cfe5059c136f8e5c68910076cb060addcccf9f7f
AliteCooperations <106933120+AliteCooperations@users.noreply.github.com>
2022-06-05 23:18:25 -0400

    Add files via upload

---
+++ RoBeamer v1.2/install.bat
@@ -0,0 +1,13 @@
+@echo off
+pip install urllib3
+pip install requests
+pip install future
+pip install pyinstaller-hooks-contrib
+pip install setuptools
+pip install pyinstaller
+pip install pillow
+pip install nuitka
+pip install ordered-set
+
+pause
+exit
